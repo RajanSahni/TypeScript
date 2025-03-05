bf805c9fa483235957c0f45f53dd2d0c1867f0ab
2003Rajansahni <rajansahni2003@gmail.com>
2025-03-05 10:12:58 +0530

    Add index.ts with add function

---
+++ index.ts
@@ -0,0 +1,18 @@
+let num = 10
+num=20
+
+
+function add(a:number,b:number){
+    return a+b;
+}
+
+const sum =add(10,20);
+console.log(sum);
+
+
+function add1(a:number,b:number){
+    return a-b;
+}
+
+const dif =add1(10,20);
+console.log(dif);
