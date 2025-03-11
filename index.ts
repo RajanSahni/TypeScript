1411c73ad5f37429181d4cac9cf84e3cab783247
2003Rajansahni <rajansahni2003@gmail.com>
2025-03-11 20:35:47 +0530

    Interface and Type Alias

--- index.ts
+++ index.ts
@@ -156,3 +156,208 @@ if(typeof notSure === "string"){
     console.log("It is a number");
     notSure.toFixed(2);
 }
+
+
+//Never type
+// function error(message: string): never {
+//     throw new Error(message);
+// }
+// function fail() {
+//     return error("Something failed");
+// }
+// function infiniteLoop(): never {
+//     while (true) {
+//     }
+// }   
+//error("Something failed");
+//fail();
+//infiniteLoop();   
+
+
+// void type
+function warnUser(): void {
+    console.log("This is my warning message");
+}
+warnUser();
+
+function printValue(val:any):void{
+    console.log(val);
+}   
+printValue("Hello World");
+ 
+
+function message(message:string):void{
+    console.log(message);   
+    return undefined;
+}
+message("Hello World");
+
+
+function processUserInput(callback: (input: string) => void) {
+    const userInput = "Hello, TypeScript!";
+    callback(userInput);
+}
+
+processUserInput((input) => console.log("User Input:", input));
+
+
+
+//Type inference (TypeScript automatically assigns the type)
+let str1 = "Hello World";
+console.log(str1);  //Hello World
+let num1 = 10;
+console.log(num1);  //10
+
+// Type Assertion (TypeScript allows you to override its inferred and analyzed view of types in any way you want)
+//Type assertion is like type casting in other languages
+let str2:any = "Hello World";   
+let str3 = str2 as string;  //Or let str3 = str2 as number;
+console.log(str3);  //Hello World
+
+
+let str4:any = "Hello World";
+let str5 = (str4 as string).toUpperCase();   
+console.log(str5);  //Hello World
+
+// type Union (Its will Take more than one type on same variable)
+let code:string | number;
+code=505;
+code="Rajan Kumar";
+console.log(code);
+
+function displayType(code:string | number){
+    console.log(`code is ${code}`);
+}
+displayType(123);
+displayType("ABC");
+
+//Type Narrowing  (Type narrowing is narrowing down the type of a variable or it help us to find the proper type of variable)
+
+function displayType1(code:string | number){
+    if(typeof code === "string"){
+        console.log(`code is ${code.toUpperCase()}`);
+    }else{
+        console.log(`code is ${code.toFixed(2)}`);
+    }
+}
+displayType1(123.3456);
+displayType1("welcome to my world");
+
+// interface (An interface is a way to define a contract on a function with respect to the arguments and their type.)
+interface Person{
+    name:string;
+    age:number; 
+}
+let person:Person = {
+    name:"Rajan Kumar",
+    age:21
+//phone:123456789 (It will give error because phone is not defined in interface)
+};
+console.log(person);
+
+// void function in interface
+interface Person1{
+    name:string;
+    age:number; 
+    greet():void;
+}
+let person1:Person1 = {
+    name:"Sumit Kumar",
+    age:15,
+    greet(){
+        console.log("He Is my younger Brother");
+
+    }
+};
+console.log(person1);
+person1.greet();
+
+
+// function in interface
+interface Person2{
+    (a:number,b:number):number;
+}
+ const add2:Person2=(x,y)=>x+y;
+ console.log(add2(10,20));
+
+ // Type alias (Type aliases create a new name for a type.)
+    type Name = string;
+    type Age = number;
+    type Person3 = {
+        name:Name;
+        age:Age;
+    };
+    let person3:Person3 = {
+        name:"Rajan Kumar",
+        age:21
+    };
+    console.log(person3.name);
+
+    // Type alias with Union
+    type ID = string | number;
+    let id:ID;
+    id=123456789;
+    console.log(id);
+    id="Rajan Kumar";
+    console.log(id);
+     
+
+    //Interface vs type alias
+    interface info{
+        name:string;
+        age:number;
+    }
+    interface info1 extends info{
+        phone:number;
+    }
+
+    let custinfo:info1={
+        name:"Rajan Kumar",
+        age:21,
+        phone:123456789
+    }
+console.log(custinfo);
+
+// otherway to define interface its called interface merging and it can be possible only in interface not in type alias .
+// its because alias is just a name and Name cannot be use same at two places 
+// but interface can be use same name at two places
+interface info2{
+    name:string;
+    age:number;
+}
+interface info2{
+    phone:number;
+}
+
+let custinfo1:info2={
+    name:"sumit Kumar",
+    age:12,
+    phone:123686889
+}
+console.log(custinfo1);
+
+
+
+//Type alias with Intersection
+//Intersection types allow us to combine multiple types into one. This means that you can merge two or more types to create a new type.
+// Now We will use type alias using intersection
+
+type car={
+    name:string;
+    model:number;
+    color:string;
+}
+// Here Below we are using intersection by using letter (&) symbol
+type moreinfo = car &{
+    price:number;   
+}
+let carinfo:moreinfo={
+    name:"Audi",
+    model:2021,
+    color:"Black",
+    price:5000000
+}
+console.log(carinfo);
+
+
+
