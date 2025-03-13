0e603bfcb337b077c8320d34a5f4ca841bb06f91
2003Rajansahni <rajansahni2003@gmail.com>
2025-03-13 23:06:30 +0530

    Add function.ts with add and divide functions

---
+++ function.ts
@@ -0,0 +1,12 @@
+console.log("Hello World");
+
+function add(a: number, b: number): number{
+    return a + b;
+}
+console.log(add(10, 20));
+
+// Function using variable 
+const divide =function (a: number, b: number): number{
+    return a/ b;
+}
+console.log(divide(90,6));
