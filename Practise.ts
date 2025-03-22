eeafc89c34b0438e13c6d22c1882b442734a2164
2003Rajansahni <rajansahni2003@gmail.com>
2025-03-22 23:08:45 +0530

    Update function in Practise.ts

--- Practise.ts
+++ Practise.ts
@@ -1,4 +1,4 @@
-//How to find factorial of number 
+//1.How to find factorial of number 
 function factorial (num:number):number{
     if(num==0 || num ==1){
         return 1;
@@ -9,6 +9,39 @@ function factorial (num:number):number{
 }
 
 console.log(factorial(5));
+
+/*
+2.define a function that accepts a user object 
+(with name , age and email ) and Returns a formatted string  
+*/
+
+function userObject(user:{name:string,age:number,email:string}):string{
+    return `Name is ${user.name} and age is ${user.age} and email is ${user.email}`;
+}
+
+console.log(userObject({name:"Shri Ram",age:25,email:" abc123@gmai.com "}));
+ //Or 
+
+
+interface UserData{
+    name:string;
+    age:number;
+    email:string;
+}
+
+function formatUser(user:UserData):string{
+    return `User ${user.name} and age is ${user.age} can be connected via ${user.email}`;        
+
+}
+
+const user:UserData = {
+
+    name:" Raj",
+    age:25,
+    email:" 22bcaf49@gmail.com"
+}
+
+console.log(formatUser(user));
 
 
 
