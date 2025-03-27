b8307476598a820e6822edaf4fc66224de32c602
2003Rajansahni <rajansahni2003@gmail.com>
2025-03-27 10:13:10 +0530

    Adding another file that is generics in Typescript

--- generics.ts
+++ generics.ts
@@ -7,7 +7,7 @@
 Generics are a way to create components(functions , classes , or interfaces) 
 that work with different types without losing type safety 
 */
-
+//generics in function
 function genericstype<T>(arg:T):T{
     return arg;
 }
@@ -38,7 +38,7 @@ function getLength<T extends {length:number}>(arg:T):number{
 
 console.log(getLength("Hello"));
 console.log(getLength([1,2,3,4,5]));//array has length property
-
+//console.log(getLength(1234));//error as number does not have length property 
 
 //generic classes
 class Box<T>{
@@ -58,3 +58,42 @@ console.log(numberBox.getContents());
 let stringBox = new Box<string>("Hello");
 console.log(stringBox.getContents());
 
+
+// stack class using generics
+class Stack<T>{
+   private items:T[]=[];
+
+    push(items:T):void{ {
+        this.items.push(items);
+    } }
+
+    pop():T | undefined{
+        return this.items.pop();
+    }
+
+    getstack():T[]{
+        return this.items;   
+    }
+}
+// string stack
+const stack = new Stack<string>();
+stack.push("Hello");
+stack.push("Rajan");
+stack.push("Kumar");
+stack.push("Sahni");
+console.log(stack.getstack());
+
+console.log(stack.pop());
+
+console.log(stack.getstack());
+ // number stack
+const numstack = new Stack<number>();
+numstack.push(1);
+numstack.push(2);
+numstack.push(4);
+numstack.push(7);
+console.log(numstack.getstack());
+
+console.log(numstack.pop());
+
+console.log(numstack.getstack());
