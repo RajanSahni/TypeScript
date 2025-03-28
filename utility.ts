c0493e330a2f6782fcc2bf5bd9a487dade02cd28
2003Rajansahni <rajansahni2003@gmail.com>
2025-03-28 23:55:48 +0530

    Update in ts

---
+++ utility.ts
@@ -0,0 +1,5 @@
+// utility functions for TypeScript
+// These functions are used to check the type of a variable at runtime.
+// They are similar to the typeof, instanceof, and in operator in TypeScript.
+
+
