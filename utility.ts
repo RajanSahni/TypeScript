c40bdcbcc8e51dc5a1deafe930e898a1bdf0d612
2003Rajansahni <rajansahni2003@gmail.com>
2025-03-30 23:07:24 +0530

    Adding more code this file

--- utility.ts
+++ utility.ts
@@ -3,3 +3,25 @@
 // They are similar to the typeof, instanceof, and in operator in TypeScript.
 
 
+class Stack2<T> {
+    private items: T[] = [];
+ 
+    push(items: T): void {
+        this.items.push(items);
+    }
+ 
+    pop(): T | undefined {
+        return this.items.pop();
+    }
+ 
+    getstack(): T[] {
+        return this.items;   
+    }
+ }
+ 
+ const stack2 = new Stack<number>();
+ stack2.push(1);
+ stack2.push(2);
+ stack2.push(3);
+ console.log(stack2.getstack());
+ console.log(stack2.pop());
